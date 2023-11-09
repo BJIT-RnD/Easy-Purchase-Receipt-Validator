9a29345bb6decdb82ac26241f5e32b87570780c0
faiaz rahman <faiaz.rahman@bjitgroup.com>
2023-11-09 17:17:19 +0600

    Load the receipt by decoding the app receipt as a PKCS #7 container
    - Implemented payload struct
    - Implemented required date formatter
    - Added description comment previous code
    - cleaned and swiftlinted the code.
    
    ref: https://redmine.bjitgroup.com/redmine/issues/488046

--- Sources/Easy-Purchase-Receipt-Validator/AppleContainer/AppleContainer.swift
+++ Sources/Easy-Purchase-Receipt-Validator/AppleContainer/AppleContainer.swift
@@ -147,5 +147,36 @@ class AppleContainer {
 }
 
 extension AppleContainer {
-    
+    public struct payloadData {
+        /// The bundle identifier of the application as specified in the Info.plist file.
+        var bundleIdentifier: String?
+
+        /// The binary representation of the bundle identifier in Info.plist, used in conjunction with other data to compute the SHA-1 hash during validation.
+        var bundleIdentifierData: Data?
+
+        /// The version of the application as specified in the Info.plist file. In iOS, it corresponds to CFBundleVersion, and in macOS, it corresponds to CFBundleShortVersionString.
+        var bundleVersion: String?
+
+        /// The original version of the application as specified in the Info.plist file. This is typically the same as the 'bundleVersion'.
+        var originalApplicationVersion: String?
+
+        /// An opaque value used in conjunction with other data to compute the SHA-1 hash during validation. This value is typically part of the validation process.
+        public var opaqueValue: Data?
+
+        /// The SHA-1 hash computed from the payload data, used to validate the receipt's integrity and authenticity.
+        public var sha1: Data?
+
+        /// The date when the receipt was created. It is a part of the receipt's metadata.
+        public var receiptCreationDate: Date?
+
+        /// A string representation of the receipt creation date, which may be useful for human-readable presentation.
+        public var receiptCreationDateString: String?
+
+        /// The date when the receipt expires, if applicable. It is a part of the receipt's metadata.
+        public var receiptExpirationDate: Date?
+
+        /// A string representation of the receipt expiration date, which may be useful for human-readable presentation.
+        public var receiptExpirationDateString: String?
+    }
 }
+
