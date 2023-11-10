d4e2f98d864b95362efce2801c55699563523555
shamin26 <mostafa.yeasar@bjitgroup.com>
2023-11-10 17:42:09 +0600

    [IAP][Receipts] Implement functionality to check app version [IAP][Receipts] Implement functionality check bundle identifier
    - Added the validate() method
    - Added the check for bundleID & appVersion with mock data
    - Added Bundle extension to fetch app version
    
    Ref-1: https://redmine.bjitgroup.com/redmine/issues/488049
    Ref-2: https://redmine.bjitgroup.com/redmine/issues/487657

--- Sources/Easy-Purchase-Receipt-Validator/InAppReceipt.swift
+++ Sources/Easy-Purchase-Receipt-Validator/InAppReceipt.swift
@@ -1,6 +1,6 @@
 //
 //  InAppReceipt.swift
-//  
+//
 //
 //  Created by BJIT on 9/11/23.
 //
@@ -11,9 +11,9 @@ import Foundation
 /// A structure representing information extracted from an in-app receipt.
 public struct InAppReceiptInfo {
     // Hardcoded values for testing
-    public fileprivate(set) var bundleIdentifier: String? = "com.example.app"
+    public fileprivate(set) var bundleIdentifier: String? = "com.bjitgroup.easypurchase"
     public fileprivate(set) var bundleIdentifierData: Data? = Data([0x01, 0x02, 0x03])
-    public fileprivate(set) var bundleVersion: String? = "1.0"
+    public fileprivate(set) var bundleVersion: String? = "1.0.0"
     public fileprivate(set) var originalApplicationVersion: String? = "1.0"
     public fileprivate(set) var opaqueValue: Data? = Data([0xAA, 0xBB, 0xCC])
     public fileprivate(set) var sha1: Data? = Data([0xDD, 0xEE, 0xFF])
@@ -73,5 +73,65 @@ public extension InAppReceipt {
     /// Property representing the original app version extracted from the in-app receipt payload.
     var originalAppVersion: String {
         return payload.originalAppVersion
+    }
+}
+
+// MARK: - InAppReceipt Validation Extension
+
+/// An extension on InAppReceipt to facilitate receipt validation.
+public extension InAppReceipt {
+
+    /// Determine whether the receipt is valid or not.
+    ///
+    /// - Returns: `true` if the receipt is valid, otherwise `false`.
+    var isValid: Bool {
+        do {
+            try validate()
+            return true
+        } catch {
+            return false
+        }
+    }
+
+    /// Validate In-App Receipt.
+    ///
+    /// - throws: An error in the InAppReceipt domain if verification fails.
+    func validate() throws {
+        try verifyBundleIdentifierAndVersion()
+    }
+
+    /// Verify In-App Receipt.
+    ///
+    /// - throws: An error in the InAppReceipt domain if verification fails.
+    @available(*, deprecated, renamed: "validate")
+    func verify() throws {
+        try verifyBundleIdentifierAndVersion()
+    }
+
+    /// Verify that the bundle identifier in the receipt matches a hard-coded constant containing the CFBundleIdentifier value you expect in the Info.plist file.
+    /// Verify that the version identifier string in the receipt matches a hard-coded constant containing the CFBundleShortVersionString value (for macOS) or the CFBundleVersion value (for iOS) that you expect in the Info.plist file.
+    ///
+    /// - throws: An error in the InAppReceipt domain if verification fails.
+    func verifyBundleIdentifierAndVersion() throws {
+        try verifyBundleIdentifier()
+        //try verifyBundleVersion()
+    }
+
+    /// Verify that the bundle identifier in the receipt matches a hard-coded constant containing the CFBundleIdentifier value you expect in the Info.plist file.
+    ///
+    /// - throws: An error in the InAppReceipt domain if verification fails.
+    func verifyBundleIdentifier() throws {
+        guard let bundleID = Bundle.main.bundleIdentifier, bundleID == bundleIdentifier else {
+            throw ValidationError.validationFailed(reason: .bundleIdentifierVerification)
+        }
+    }
+
+    /// Verify that the version identifier string in the receipt matches a hard-coded constant containing the CFBundleShortVersionString value (for macOS) or the CFBundleVersion value (for iOS) that you expect in the Info.plist file.
+    ///
+    /// - throws: An error in the InAppReceipt domain if verification fails.
+    func verifyBundleVersion() throws {
+        guard let version = Bundle.main.appVersion, version == appVersion else {
+            throw ValidationError.validationFailed(reason: .bundleVersionVerification)
+        }
     }
 }
