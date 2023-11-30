6ffc3f9394fb205c743ddd3a0c0a1e0ab381fcf6
Rejaul Hasan <rejaul.hasan@bjitgroup.com>
2023-11-30 14:00:17 +0600

    Signature Validation Implementation - Implement X509PublicKey class. - Hold the data model and parse properly. Ref: https://redmine.bjitgroup.com/redmine/issues/489595

--- Sources/Easy-Purchase-Receipt-Validator/X509/X509PublicKey.swift
+++ Sources/Easy-Purchase-Receipt-Validator/X509/X509PublicKey.swift
@@ -20,5 +20,44 @@
 
 import Foundation
 class X509PublicKey {
+    let pkBlock: ASN1Object
+
+    init(pkBlock: ASN1Object) {
+        self.pkBlock = pkBlock
+    }
+    var algOid: String? {
+        return pkBlock.childASN1Object(at: 0)?.childASN1Object(at: 0)?.value as? String
+    }
+    var algName: String? {
+        return OID.description(of: algOid ?? "")
+    }
+    var algParams: String? {
+        return pkBlock.childASN1Object(at: 0)?.childASN1Object(at: 1)?.value as? String
+    }
     
+    var key: Data? {
+        guard
+            let algOid = algOid,
+            let oid = OID(rawValue: algOid),
+            let keyData = pkBlock.childASN1Object(at: 1)?.value as? Data else {
+                return nil
+        }
+
+        switch oid {
+        case .ecPublicKey:
+            return keyData
+
+        case .rsaEncryption:
+            let decoder = ASN1Decoder()
+            guard let publicKeyAsn1Objects = (try? decoder.decode(data: keyData)) else {
+                return nil
+            }
+            guard let publicKeyModulus = publicKeyAsn1Objects.first?.childASN1Object(at: 0)?.value as? Data else {
+                return nil
+            }
+            return publicKeyModulus
+        default:
+            return nil
+        }
+    }
 }
