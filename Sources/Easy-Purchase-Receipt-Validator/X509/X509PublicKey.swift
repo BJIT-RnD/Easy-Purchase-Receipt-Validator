9be62c78d1ca3fdab22ff944efc159de6f72b9c3
Rejaul Hasan <rejaul.hasan@bjitgroup.com>
2023-11-30 13:56:40 +0600

    Signature Validation Implementation - Create X509PublicKey class - Define a sample public key ASN1Object. Ref: https://redmine.bjitgroup.com/redmine/issues/489595

---
+++ Sources/Easy-Purchase-Receipt-Validator/X509/X509PublicKey.swift
@@ -0,0 +1,24 @@
+/**
+ File Name: `X509PublicKey.swift`
+ 
+ Description: This file contains the implementation of the `X509PublicKey` class, which is responsible for modeling and parsing X509PublicKey.
+ 
+ Author: Md. Rejaul Hasan
+ 
+ © 2023 BJIT. All rights reserved.
+ */
+/* Sample example
+ 
+ SEQUENCE{
+     SEQUENCE{
+         OBJECTIDENTIFIER: 1.2.840.113549.1.1.1 (rsaEncryption)
+         NULL
+     }
+     BITSTRING: 270 bytes
+ }
+*/
+
+import Foundation
+class X509PublicKey {
+    
+}
