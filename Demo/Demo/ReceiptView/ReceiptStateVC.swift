e64da56c61f0c3eb51022d8eac3384fc93771200
faiaz rahman <faiaz.rahman@bjitgroup.com>
2023-12-08 18:02:58 +0600

    Demo App Implementations - Added Screen to read receipt interface elements
    ref: https://redmine.bjitgroup.com/redmine/issues/493335

--- Demo/Demo/ReceiptView/ReceiptStateVC.swift
+++ Demo/Demo/ReceiptView/ReceiptStateVC.swift
@@ -12,9 +12,9 @@ final class ReceiptStateVC: UIViewController {
     @IBOutlet weak var availabilityBox: UIView!
     @IBOutlet weak var availabilityLabel: UILabel!
     @IBOutlet weak var loader: UIActivityIndicatorView!
+    @IBOutlet weak var receiptPageOutlet: UIButton!
     var doesReceiptExist = false
     static var viewModel = ReceiptStateViewModel()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         initialSetup()
@@ -23,6 +23,8 @@ final class ReceiptStateVC: UIViewController {
     private func initialSetup() {
         availabilityBox.layer.cornerRadius = 10
         refreshBtn.layer.cornerRadius = 10
+        receiptPageOutlet.layer.cornerRadius = 10
+        receiptPageOutlet.isHidden = true
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -36,7 +38,18 @@ final class ReceiptStateVC: UIViewController {
                     self?.availabilityBox.backgroundColor = UIColor(red: 0, green: 0.45, blue: 0.13, alpha: 1)
                 }
                 self?.availabilityLabel.text = "Available"
+                self?.receiptPageOutlet.isHidden = false
             }
+        }
+    }
+    @IBAction func receiptPageAction(_ sender: Any) {
+        if let destinationVC = storyboard?.instantiateViewController(withIdentifier: "ReceiptData") as? ReceiptData {
+            destinationVC.hidesBottomBarWhenPushed = true
+//            guard let product = Constants.availableProductsCollection[productList[indexPath.row].payment.productIdentifier] else {
+//                return
+//            }
+//            destinationVC.product = product
+            navigationController?.pushViewController(destinationVC, animated: true)
         }
     }
     @IBAction func refreshReceipt(_ sender: Any) {
